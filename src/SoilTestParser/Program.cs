a9609a2bd4fe33cc6539428221ec9d9bb0f5fa87
1AMAC0MPU73R <Andrew.Gauvin@outlook.com>
2021-05-29 12:29:41 -0400

    Initial Commit

---
+++ src/SoilTestParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoilTestParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
